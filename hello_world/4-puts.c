8359eb1b123027ce2dac1dd592ae189cf7634e6a
XcannesOlivier <desruelleolivier@gmail.com>
2025-10-20 23:07:01 +0200

    first commit 4-putsGOOD1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * Bien Vu le piége de l'apostrophe
+ *
+ * puts mais un espace à la fin automatiquement
+ */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
