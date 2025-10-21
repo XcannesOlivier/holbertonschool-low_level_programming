b4ce4700538fa0db3ac76f5646773d24993ed4f4
XcannesOlivier <desruelleolivier@gmail.com>
2025-10-21 10:32:16 +0200

    first commit 5-printfGOOOD

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,11 +5,10 @@
  *
  * Bien vu le piége virgule.
  * "Programming is like building a multilingual puzzle
- * 
  *
  * Return: 0 (ok)
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
