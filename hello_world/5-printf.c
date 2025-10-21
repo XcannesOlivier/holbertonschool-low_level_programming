c859961424d22b4bbed6b889275ddbd5ff46d38f
XcannesOlivier <desruelleolivier@gmail.com>
2025-10-21 09:54:38 +0200

    first commit 5-printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * écrire une phrase avec printf
+ *
+ * Bien vu le piége virgule.
+ * "Programming is like building a multilingual puzzle
+ * 
+ *
+ * Return: 0 (ok)
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
