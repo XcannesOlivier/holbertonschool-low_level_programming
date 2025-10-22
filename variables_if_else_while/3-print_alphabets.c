889fa15eaf5fd4131f6a83764c62d6025d90bae7
XcannesOlivier <desruelleolivier@gmail.com>
2025-10-22 13:07:50 +0200

    Initial commit 3-print_alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * main - Prints all alĥabet in lower
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+        putchar(c);
+     
+	for (c = 'A'; c <= 'Z'; c++)
+        putchar(c);
+	putchar('\n');
+	return (0);
+}
