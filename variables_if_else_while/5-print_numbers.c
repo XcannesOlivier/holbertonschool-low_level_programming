e774f9a677b12d0a98c24b8e5a6180f4132ae256
XcannesOlivier <desruelleolivier@gmail.com>
2025-10-22 14:03:34 +0200

    Initial commit 5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * main - Prints all single digit number
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+
+	putchar('\n');
+	return (0);
+}
