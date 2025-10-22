91c31c35b2cc9a6d29b8e6203e5df3c36fa34aac
XcannesOlivier <desruelleolivier@gmail.com>
2025-10-22 16:34:24 +0200

    Initial commit 8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * main - Prints all single digit number
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int  n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n + '0' );
+	putchar('\n');
+
+	for (n = 'a'; n <= 'f'; n++) 
+	return (0);
+}
