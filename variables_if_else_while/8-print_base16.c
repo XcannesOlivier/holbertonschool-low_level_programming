950294e8fedd6f4d72d0adaf0d766de6f3c36c2b
XcannesOlivier <desruelleolivier@gmail.com>
2025-10-22 17:06:07 +0200

    Initial commit 8-print_base16b1

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -2,20 +2,22 @@
 
 /**
  * main - Entry point
- * main - Prints all single digit number
+ * Prints all single digit numbers of base 16
  * Return: Always 0
  */
-
 int main(void)
 {
-	int  n;
+	int n;
 
-	for (n = '0'; n <= '9'; n++)
-	putchar(n + '0' );
+    /* chiffres 0 à 9 */
+	for (n = 0; n < 10; n++)
+	putchar(n + '0');
 
+    /* lettres a à f */
 	for (n = 'a'; n <= 'f'; n++)
 	putchar(n);
 
-	putchar('\n'); 
+	putchar('\n');
 	return (0);
 }
+
