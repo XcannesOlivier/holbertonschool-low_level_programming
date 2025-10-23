522c07c6757c82f4b9379d16709e31795a4b5cf3
XcannesOlivier <desruelleolivier@gmail.com>
2025-10-23 12:11:58 +0200

    first 9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints all single-digit numbers separated by comma and space
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+	putchar(n);
+
+	if (n != '9')  /* Si ce n’est pas le dernier chiffre */
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+
+	putchar('\n'); /* retour à la ligne à la fin */
+   	return (0);
+}
+
